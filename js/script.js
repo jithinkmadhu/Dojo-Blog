86f25d4073b4a9e1d137df639795bdbfcba74317
jithinkmadhu <jithinmadhu00@gmail.com>
2022-08-26 10:07:27 +0530

    Optimized

--- js/script.js
+++ js/script.js
@@ -1,19 +1,28 @@
 var blogs = [];
-let blog_id = -1;
-function toggle(input) {
-  let get_edit_body = document.getElementById("edit-body");
-  let get_add_body = document.getElementById("add-body");
-  let get_each_body = document.getElementById("each-body");
-  let get_home_body = document.getElementById("home-body");
-  let get_title = document.getElementById("title");
-  let get_body = document.getElementById("body");
-  let get_home_button = document.getElementById("home-button");
-  let get_new_blog_button = document.getElementById("new-blog-button");
-  let get_author = document.getElementById("author");
-  let get_blogs_list = document.getElementById("blogs-list");
+let get_title = document.getElementById("title");
+let get_body = document.getElementById("body");
+let get_author = document.getElementById("author");
+let get_edit_button = document.getElementById("edit-btn");
+let get_delete_button = document.getElementById("delete-btn");
+let get_edit_title = document.getElementById("edit-title");
+let get_edit_blog_body = document.getElementById("edit-blog-body");
+let get_edit_author = document.getElementById("edit-author");
+let get_update_button = document.getElementById("update-button");
+let get_each_body = document.getElementById("each-body");
+let get_home_body = document.getElementById("home-body");
+let get_add_body = document.getElementById("add-body");
+let get_edit_body = document.getElementById("edit-body");
+let get_blogs_list = document.getElementById("blogs-list");
+let get_title_error = document.getElementById("edit-title-error");
+let get_body_error = document.getElementById("edit-body-error");
+let get_home_button = document.getElementById("home-button");
+let get_new_blog_button = document.getElementById("new-blog-button");
+let get_each_title = document.getElementById("edit-title");
+let get_each_blog_body = document.getElementById("edit-blog-body");
+let get_each_author = document.getElementById("edit-author");
 
+function toggle(input) {
   if (input == "Home") {
-    //alert("You are leaving add blog page");
     get_edit_body.style.display = "none";
     get_add_body.style.display = "none";
     get_each_body.style.display = "none";
@@ -21,28 +30,8 @@ function toggle(input) {
     get_new_blog_button.classList.add("nav-button");
     get_home_button.classList.remove("nav-button");
 
-    while (get_blogs_list.firstChild) {
-      get_blogs_list.removeChild(get_blogs_list.firstChild);
-    }
-
-    for (let i = 0; i < blogs.length; i++) {
-      const addli = document.createElement("li");
-      const innerp1 = document.createElement("p");
-      const innerp2 = document.createElement("p");
-      const innerpContent1 = document.createTextNode(blogs[i].blog_title);
-      const innerpContent2 = document.createTextNode(
-        "written by " + blogs[i].blog_author
-      );
-      innerp1.appendChild(innerpContent1);
-      innerp2.appendChild(innerpContent2);
-      addli.appendChild(innerp1).className = "pink-color title";
-      addli.appendChild(innerp2).className = "author";
-      addli.id = i;
-      addli.setAttribute("onclick", "eachItem(id)");
-      get_blogs_list.appendChild(addli);
-    }
+    display();
   } else if (input == "Blog") {
-    //alert("You are leaving home page");
     get_add_body.style.display = "block";
     get_home_body.style.display = "none";
     get_each_body.style.display = "none";
@@ -54,18 +43,6 @@ function toggle(input) {
   }
 }
 function validate() {
-  let get_add_body = document.getElementById("add-body");
-  let get_each_body = document.getElementById("each-body");
-  let get_home_body = document.getElementById("home-body");
-  let get_title = document.getElementById("title");
-  let get_body = document.getElementById("body");
-  let get_title_error = document.getElementById("title-error");
-  let get_body_error = document.getElementById("body-error");
-  let get_author = document.getElementById("author");
-  let get_blogs_list = document.getElementById("blogs-list");
-  let get_home_button = document.getElementById("home-button");
-  let get_new_blog_button = document.getElementById("new-blog-button");
-
   let title = get_title.value;
   let body = get_body.value;
   let author = get_author.value;
@@ -98,37 +75,11 @@ function validate() {
   get_new_blog_button.classList.add("nav-button");
   get_home_button.classList.remove("nav-button");
 
-  while (get_blogs_list.firstChild) {
-    get_blogs_list.removeChild(get_blogs_list.firstChild);
-  }
-
-  for (let i = 0; i < blogs.length; i++) {
-    const addli = document.createElement("li");
-    const innerp1 = document.createElement("p");
-    const innerp2 = document.createElement("p");
-    const innerpContent1 = document.createTextNode(blogs[i].blog_title);
-    const innerpContent2 = document.createTextNode(
-      "written by " + blogs[i].blog_author
-    );
-    innerp1.appendChild(innerpContent1);
-    innerp2.appendChild(innerpContent2);
-    addli.appendChild(innerp1).className = "pink-color title";
-    addli.appendChild(innerp2).className = "author";
-    addli.id = i;
-    addli.setAttribute("onclick", "eachItem(id)");
-    get_blogs_list.appendChild(addli);
-  }
+  display();
 
   return true;
 }
 function eachItem(num) {
-  let get_each_body = document.getElementById("each-body");
-  let get_home_body = document.getElementById("home-body");
-  let get_edit_button = document.getElementById("edit-btn");
-  let get_delete_button = document.getElementById("delete-btn");
-  let get_home_button = document.getElementById("home-button");
-  let get_new_blog_button = document.getElementById("new-blog-button");
-
   document.getElementById("each-blog-title").innerHTML = blogs[num].blog_title;
   document.getElementById("each-blog-author").innerHTML =
     "written by " + blogs[num].blog_author;
@@ -146,17 +97,6 @@ function eachItem(num) {
   get_home_button.classList.add("nav-button");
 }
 function editBlog(num) {
-  let get_edit_body = document.getElementById("edit-body");
-  let get_add_body = document.getElementById("add-body");
-  let get_each_body = document.getElementById("each-body");
-  let get_home_body = document.getElementById("home-body");
-  let get_edit_title = document.getElementById("edit-title");
-  let get_edit_blog_body = document.getElementById("edit-blog-body");
-  let get_home_button = document.getElementById("home-button");
-  let get_new_blog_button = document.getElementById("new-blog-button");
-  let get_edit_author = document.getElementById("edit-author");
-  let get_update_button = document.getElementById("update-button");
-
   get_edit_body.style.display = "block";
   get_add_body.style.display = "none";
   get_home_body.style.display = "none";
@@ -172,13 +112,6 @@ function editBlog(num) {
 }
 
 function deleteBlog(num) {
-  let get_each_body = document.getElementById("each-body");
-  let get_home_body = document.getElementById("home-body");
-  let get_add_body = document.getElementById("add-body");
-  let get_home_button = document.getElementById("home-button");
-  let get_new_blog_button = document.getElementById("new-blog-button");
-  let get_blogs_list = document.getElementById("blogs-list");
-
   blogs.splice(num, 1);
   console.log(blogs);
   get_add_body.style.display = "none";
@@ -187,44 +120,10 @@ function deleteBlog(num) {
   get_new_blog_button.classList.add("nav-button");
   get_home_button.classList.remove("nav-button");
 
-  while (get_blogs_list.firstChild) {
-    get_blogs_list.removeChild(get_blogs_list.firstChild);
-  }
-
-  for (let i = 0; i < blogs.length; i++) {
-    const addli = document.createElement("li");
-    const innerp1 = document.createElement("p");
-    const innerp2 = document.createElement("p");
-    const innerpContent1 = document.createTextNode(blogs[i].blog_title);
-    const innerpContent2 = document.createTextNode(
-      "written by " + blogs[i].blog_author
-    );
-    innerp1.appendChild(innerpContent1);
-    innerp2.appendChild(innerpContent2);
-    addli.appendChild(innerp1).className = "pink-color title";
-    addli.appendChild(innerp2).className = "author";
-    addli.id = i;
-    addli.setAttribute("onclick", "eachItem(id)");
-    get_blogs_list.appendChild(addli);
-  }
+  display();
 }
 
 function update(num) {
-  let get_each_body = document.getElementById("each-body");
-  let get_home_body = document.getElementById("home-body");
-  let get_add_body = document.getElementById("add-body");
-  let get_blogs_list = document.getElementById("blogs-list");
-  let get_edit_body = document.getElementById("edit-body");
-
-  let get_title_error = document.getElementById("edit-title-error");
-  let get_body_error = document.getElementById("edit-body-error");
-  let get_home_button = document.getElementById("home-button");
-  let get_new_blog_button = document.getElementById("new-blog-button");
-
-  let get_each_title = document.getElementById("edit-title");
-  let get_each_blog_body = document.getElementById("edit-blog-body");
-  let get_each_author = document.getElementById("edit-author");
-
   get_edit_body.style.display = "none";
   get_title_error.style.display = "none";
   get_body_error.style.display = "none";
@@ -253,6 +152,12 @@ function update(num) {
   get_new_blog_button.classList.add("nav-button");
   get_home_button.classList.remove("nav-button");
 
+  display();
+
+  return true;
+}
+
+function display() {
   while (get_blogs_list.firstChild) {
     get_blogs_list.removeChild(get_blogs_list.firstChild);
   }
@@ -273,6 +178,4 @@ function update(num) {
     addli.setAttribute("onclick", "eachItem(id)");
     get_blogs_list.appendChild(addli);
   }
-
-  return true;
 }
